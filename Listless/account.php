1ff794bb07ccd0cdeb51d9e7b031a9d30f55915f
lirmanis <ip25.k.lirmanis@vtdt.edu.lv>
2026-05-22 10:14:01 +0300

    Add files via upload

---
+++ Listless/account.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Main/style.css">
+
+</head>
+<body>
+
+    <div class="container">
+       <div class="UpperTab">
+        <div class="cont1">
+            <img class="pageIcon" src="https://cdn-icons-png.flaticon.com/256/8194/8194907.png">
+            <p>Listless</p>
+        </div>
+        <div class="cont2">
+            <p id="level"> Lv. 1 </p>
+        </div>
+            
+       </div>
+    </div>
+
+</body>
+</html>
